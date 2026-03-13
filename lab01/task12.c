419712aae69346e24420dc1fde9a7ee93d740925
LisaL <liza2710lizale@gmail.com>
2026-03-13 21:05:08 +0000

    Сделала Лабораторную работу 1(lab01)

---
+++ lab01/task12.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    
+    printf("Введите трехзначное число: ");
+    scanf("%d", &num);
+    
+    if (num < 100 || num > 999) {
+        printf("Ошибка: число должно быть трехзначным!\n");
+        return 1;
+    }
+    
+    int hundreds = num / 100;
+    int tens = (num / 10) % 10;
+    int ones = num % 10;
+    
+    printf("Сотни: %d, Десятки: %d, Единицы: %d\n", hundreds, tens, ones);
+    
+    return 0;
+}
