3453a519c1fb8f8d4189b1b7350468c6b4480a6b
LisaL <liza2710lizale@gmail.com>
2026-04-01 10:03:23 +0000

    Добавила лабораторную работу 1

---
+++ lab01/lab01_temp/task2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("sizeof int: %lu\n", sizeof(int));
+	printf("sizeof char: %lu\n", sizeof(char));
+	printf("sizeof float: %lu\n", sizeof(float));
+	printf("sizeof double %lu\n", sizeof(double));
+	return 0;
+}
